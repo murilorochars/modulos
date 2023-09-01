2623ced7c3287f2dbf2c48e926b792c42268c6e7
Murilo Rocha da Rosa <ofcsmurilo@gmail.com>
2023-09-01 08:54:22 -0300

    Versao com função nome

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,11 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var multiplicar = require("./modulos/multiplicar");
 var dividir = require("./modulos/dividir");
+var mensagem = require("./modulos/mensagem");
+var nome = "Murilo"
 
 console.log(somar(20,90));
 console.log(media(20,90));
 console.log(multiplicar(20,90));
 console.log(dividir(100,10));
+console.log(mensagem (nome))
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+    return "ola " + nome + " inter vai ser campeoao";
+  }; 
+  module.exports = mensagem;
